42634ffd6d22bc622bca8c95fd8fe469bc160277
Alfisha-Anjum <anjumalfisha@gmail.com>
2024-07-31 18:19:50 +0530

    commit

--- script.js
+++ script.js
@@ -7,3 +7,24 @@ function changeContent(title, imageUrl, content) {
     "healthcare"
   ).style.backgroundImage = `url(${imageUrl})`;
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  const expertiseSelect = document.getElementById("expertise");
+
+  expertiseSelect.addEventListener("change", (event) => {
+    if (event.target.value === "all") {
+      if (event.target.selectedOptions.length === 1) {
+        // Select all options if "Select All" is the only selected option
+        Array.from(event.target.options).forEach(
+          (option) => (option.selected = true)
+        );
+      } else {
+        // Deselect "Select All" if other options are selected
+        event.target.options[0].selected = false;
+      }
+    } else {
+      // Deselect "Select All" if other options are selected
+      event.target.options[0].selected = false;
+    }
+  });
+});
